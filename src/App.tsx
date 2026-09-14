6c4affea0a01f5ea132b9966de869053049e36cd
Thiago <thiaguu890@gmail.com>
2026-09-14 14:47:31 -0300

    style: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
-
     return (
         <>
-            <Heading>
-                Olá Mundo 1
-                <button>
-                    <TimerIcon/>
-                </button>
-            </Heading>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero totam quisquam veniam inventore delectus perferendis, animi molestias omnis mollitia corrupti cum veritatis eum culpa sunt nostrum doloribus repellat eaque minima.
-            </p>
+            <div className="container">
+                <div className="content">
+                    <section>LOGO</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>MENU</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>LOGO</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>FORM</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>FOOTER</section>
+                </div>
+            </div>
         </>
     )
 }
