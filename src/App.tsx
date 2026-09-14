baeb03934313a844946a66826b1360db14839a2a
Thiago <thiaguu890@gmail.com>
2026-09-14 13:35:44 -0300

    style: setting the display

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css'
 import './styles/global.css'
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-    console.log('Oi');
 
     return (
         <>
-            <Heading  attr={123} attr2='string'>Olá Hipérbole 1</Heading>
-            <Heading>Olá Hipérbole 2</Heading>
-            <Heading>Olá Hipérbole 3</Heading>
+            <Heading>
+                Olá Mundo 1
+                <button>
+                    <TimerIcon/>
+                </button>
+            </Heading>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero totam quisquam veniam inventore delectus perferendis, animi molestias omnis mollitia corrupti cum veritatis eum culpa sunt nostrum doloribus repellat eaque minima.
             </p>
