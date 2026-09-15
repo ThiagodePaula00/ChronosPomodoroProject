d9c9fe6aafb3ed5798b5a4780d9bb6154a6d4ab8
Thiago <thiaguu890@gmail.com>
2026-09-15 08:55:25 -0300

    style: implementing the buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import './styles/theme.css'
 import './styles/global.css'
 
-import { Heading } from './components/Heading'
 import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
 
 export function App() {
     return (
         <>
             <Container>
-                    <Heading>Logo</Heading>
+                <Logo/>
             </Container>
             
             <Container>
-                    <Heading>Menu</Heading>
+                <Menu/>
+            </Container>
+
+            <Container>
+                <CountDown/>
+            </Container>
+
+            <Container>
+                <form className='form' action="">
+                    <div className="formFow">
+                        <label htmlFor="input">task</label>
+                        <input id='input' type="text" />
+                    </div>
+
+                    <div className="formFow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formFow">
+                        <p>Ciclos</p>
+                    </div>
+
+                    <div className="formFow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
