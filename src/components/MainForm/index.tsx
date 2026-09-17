65d4d5daa3bfc2727a783d1ab222fac9f6a43530
Thiago <thiaguu890@gmail.com>
2026-09-17 14:17:14 -0300

    style: implement cycle rendering in Cycles component and add utility for formatting seconds to minutes

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import type { TaskModel } from "../../models/TaskModel";
 import { getNextCycle } from "../utils/getNextCycle";
 import { getNextCycleType } from "../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../utils/formatSecondsToMinutes";
 
 export function MainForm() {
     const { state, setState } = useTaskContext()
@@ -35,11 +36,11 @@ export function MainForm() {
             startDate: Date.now(),
             completeDate: null,
             interruptDate: null,
-            duration: 1,
+            duration: state.config[nextCycleType],
             type: nextCycleType,
         };
 
-        const secondsRemaining = newTask.duration * 60
+        const secondsRemaining = newTask.duration * 60;
 
         setState(prevState => {
             return {
@@ -48,11 +49,11 @@ export function MainForm() {
                 activeTask: newTask,
                 currentCycle: nextCycle,
                 secondsRemaining,
-                formattedSecondsReamining: '00:00',
+                formattedSecondsReamining: formatSecondsToMinutes(secondsRemaining),
                 tasks: [...prevState.tasks, newTask],
             };
         });
-    }
+    };
     
     return (
         <form onSubmit={handleCreateNewTask} className='form' action="">
@@ -64,9 +65,11 @@ export function MainForm() {
             <p>Próximo intervalo é de 25min</p>
         </div>
 
+        {state.currentCycle === 0 && (
         <div className="formRow">
             <Cycles />
         </div>
+        )}
 
         <div className="formRow">
             <DefaultButton icon={ <PlayCircleIcon />} />
